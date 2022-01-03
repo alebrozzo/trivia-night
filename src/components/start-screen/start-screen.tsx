667b64eccaabb8039dc9dc975ec6dd8d381e25ff
Alejandro Brozzo <alebrozzo@msn.com>
2022-01-03 15:07:45 +1300

    Removed default app, added first component

---
+++ src/components/start-screen/start-screen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./start-screen.css";
+
+interface Props {}
+
+const StartScreen: React.FC<Props> = ({}) => {
+  return (
+    <>
+      <h1>Trivia Night!</h1>
+    </>
+  );
+};
+
+export { StartScreen };
